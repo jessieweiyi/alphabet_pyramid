2a21687219e090042e115fe73c0c376f83ad969d
Jessie Wei <jessieweiyi@gmail.com>
2016-01-27 19:33:37 +1100

    reformatted the code

--- ruby/alphabet_pyramid.rb
+++ ruby/alphabet_pyramid.rb
@@ -1,45 +1,45 @@
 class AlphabetPyramid
 
   attr_accessor :int_alphabet
-	
+
   UPPER_A = 'A'.ord
   UPPER_LOWER_DIFF = 'a'.ord - 'A'.ord
 
-	def initialize(alphabet)
-		raise 'INVALID INPUT' unless alphabet[/^[a-zA-Z]$/]
+  def initialize(alphabet)
+    raise 'INVALID INPUT' unless alphabet[/^[a-zA-Z]$/]
 
-		@int_alphabet = alphabet.ord
+    @int_alphabet = alphabet.ord
 
-		if @int_alphabet > 90 then
-			@int_alphabet = @int_alphabet - UPPER_LOWER_DIFF
-		end
-	end
+    if @int_alphabet > 90 then
+      @int_alphabet = @int_alphabet - UPPER_LOWER_DIFF
+    end
+  end
 
   def print_pyramid
-		line_number = 1;
-		edge = @int_alphabet + 1 - UPPER_A;
-		direction = +1;
-		print_line(line_number, edge, direction);
-	end
+    line_number = 1;
+    edge = @int_alphabet + 1 - UPPER_A;
+    direction = +1;
+    print_line(line_number, edge, direction);
+  end
 
-	def print_line(line_number, edge, direction)
-		return nil if line_number == 0
-		c = (UPPER_A + line_number - 1).chr
-		l = Array.new(line_number+edge-1)
+  def print_line(line_number, edge, direction)
+    return nil if line_number == 0
+    c = (UPPER_A + line_number - 1).chr
+    l = Array.new(line_number+edge-1)
 
-		i = 0
-		while i < l.length
-			l[i] = ' ';
-			i += 1;
-		end
+    i = 0
+    while i < l.length
+      l[i] = ' ';
+      i += 1;
+    end
 
-		l[line_number+edge-2] = c
-		l[edge-line_number] = c
+    l[line_number+edge-2] = c
+    l[edge-line_number] = c
 
-		puts l.join
+    puts l.join
 
-		direction = -1 * direction if line_number == edge
+    direction = -1 * direction if line_number == edge
 
-		print_line(line_number + direction, edge, direction)
-	end
+    print_line(line_number + direction, edge, direction)
+  end
 end
--- ruby/tc_alphabet_pyramid.rb
+++ ruby/tc_alphabet_pyramid.rb
@@ -1,89 +1,89 @@
 require_relative "alphabet_pyramid"
 require "minitest/autorun"
- 
+
 class TestAlphabetPyramid < MiniTest::Test
 
   EXPECTED_FOR_A = ['A']
 
   EXPECTED_FOR_E = [
-              '    A',
-              '   B B',
-              '  C   C',
-              ' D     D',
-              'E       E',
-              ' D     D',
-              '  C   C',
-              '   B B',
-              '    A']
+      '    A',
+      '   B B',
+      '  C   C',
+      ' D     D',
+      'E       E',
+      ' D     D',
+      '  C   C',
+      '   B B',
+      '    A']
 
   EXPECTED_FOR_G = [
-              '      A',
-              '     B B',
-              '    C   C',
-              '   D     D',
-              '  E       E',
-              ' F         F',
-              'G           G',
-              ' F         F',
-              '  E       E',
-              '   D     D',
-              '    C   C',
-              '     B B',
-              '      A']
+      '      A',
+      '     B B',
+      '    C   C',
+      '   D     D',
+      '  E       E',
+      ' F         F',
+      'G           G',
+      ' F         F',
+      '  E       E',
+      '   D     D',
+      '    C   C',
+      '     B B',
+      '      A']
 
   EXPECTED_FOR_Z = [
-              '                         A',
-              '                        B B',
-              '                       C   C',
-              '                      D     D',
-              '                     E       E',
-              '                    F         F',
-              '                   G           G',
-              '                  H             H',
-              '                 I               I',
-              '                J                 J',
-              '               K                   K',
-              '              L                     L',
-              '             M                       M',
-              '            N                         N',
-              '           O                           O',
-              '          P                             P',
-              '         Q                               Q',
-              '        R                                 R',
-              '       S                                   S',
-              '      T                                     T',
-              '     U                                       U',
-              '    V                                         V',
-              '   W                                           W',
-              '  X                                             X',
-              ' Y                                               Y',
-              'Z                                                 Z',
-              ' Y                                               Y',
-              '  X                                             X',
-              '   W                                           W',
-              '    V                                         V',
-              '     U                                       U',
-              '      T                                     T',
-              '       S                                   S',
-              '        R                                 R',
-              '         Q                               Q',
-              '          P                             P',
-              '           O                           O',
-              '            N                         N',
-              '             M                       M',
-              '              L                     L',
-              '               K                   K',
-              '                J                 J',
-              '                 I               I',
-              '                  H             H',
-              '                   G           G',
-              '                    F         F',
-              '                     E       E',
-              '                      D     D',
-              '                       C   C',
-              '                        B B',
-              '                         A']
- 
+      '                         A',
+      '                        B B',
+      '                       C   C',
+      '                      D     D',
+      '                     E       E',
+      '                    F         F',
+      '                   G           G',
+      '                  H             H',
+      '                 I               I',
+      '                J                 J',
+      '               K                   K',
+      '              L                     L',
+      '             M                       M',
+      '            N                         N',
+      '           O                           O',
+      '          P                             P',
+      '         Q                               Q',
+      '        R                                 R',
+      '       S                                   S',
+      '      T                                     T',
+      '     U                                       U',
+      '    V                                         V',
+      '   W                                           W',
+      '  X                                             X',
+      ' Y                                               Y',
+      'Z                                                 Z',
+      ' Y                                               Y',
+      '  X                                             X',
+      '   W                                           W',
+      '    V                                         V',
+      '     U                                       U',
+      '      T                                     T',
+      '       S                                   S',
+      '        R                                 R',
+      '         Q                               Q',
+      '          P                             P',
+      '           O                           O',
+      '            N                         N',
+      '             M                       M',
+      '              L                     L',
+      '               K                   K',
+      '                J                 J',
+      '                 I               I',
+      '                  H             H',
+      '                   G           G',
+      '                    F         F',
+      '                     E       E',
+      '                      D     D',
+      '                       C   C',
+      '                        B B',
+      '                         A']
+
   def test_invalid_input
     assert_raises(RuntimeError, 'INVALID_INPUT') do
       AlphabetPyramid.new('*')
@@ -108,10 +108,10 @@ class TestAlphabetPyramid < MiniTest::Test
 
     assert_equal(90, AlphabetPyramid.new('Z').int_alphabet)
     assert_equal(90, AlphabetPyramid.new('z').int_alphabet)
-  end 
+  end
 
   def test_uppercase
-   actual = with_captured_stdout_as_array { AlphabetPyramid.new('E').print_pyramid }
+    actual = with_captured_stdout_as_array { AlphabetPyramid.new('E').print_pyramid }
 
     assert_equal(EXPECTED_FOR_E, actual);
   end
@@ -149,7 +149,7 @@ class TestAlphabetPyramid < MiniTest::Test
   def with_captured_stdout_as_array
     begin
       old_stdout = $stdout
-      $stdout = StringIO.new('','w')
+      $stdout = StringIO.new('', 'w')
       yield
       $stdout.string.split(/\n+/)
     ensure
